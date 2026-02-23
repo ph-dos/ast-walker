d8da6c19eabcc0920d51b8564f0df8e9805227cf
ph-dos <phm4860@uga.edu>
2026-02-22 22:39:05 -0600

    feat: initial project setup
    Did a rust rewrite of the go lexer and naively chose Logos as a dependency
    because it made things very simple. I might need to refactor it later.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,63 @@
+use logos::Logos;
+
+use std::{
+    env,
+    fs::File,
+    io::{self, Read, Write},
+    process::exit,
+};
+
+#[derive(Logos, Debug, PartialEq)]
+enum Token {
+    // #[token(";")]
+    // Semicolon,
+    #[regex(r"[a-zA-Z_][a-zA-Z0-9_]*")]
+    Identifier,
+
+    #[regex(r#""([^"\\]|\\.)*""#)]
+    StringLiteral,
+
+    #[regex(r"[ \t\n\f]+", logos::skip)]
+    Whitespace,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let args_len = args.len();
+    if args_len > 2 {
+        println!("Usage: lox [filepath]");
+        exit(64);
+    } else if args_len == 2 {
+        run_file(&args[1]);
+    } else if args_len == 1 {
+        run_prompt();
+    }
+}
+
+fn run_file(filepath: &str) {
+    let mut file = File::open(filepath).unwrap();
+    let mut buf = String::new();
+    file.read_to_string(&mut buf).unwrap();
+    run(&buf);
+}
+
+fn run_prompt() {
+    let mut buf = String::new();
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
+        let bytes = io::stdin().read_line(&mut buf).unwrap();
+        if bytes == 0 {
+            break;
+        }
+        run(&buf);
+        buf.clear();
+    }
+}
+
+fn run(source: &str) {
+    let mut lexer = Token::lexer(source);
+    while let Some(_token) = lexer.next() {
+        println!("{}", lexer.slice());
+    }
 }
